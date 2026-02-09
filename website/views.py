8111ca6d65f2a01947e1f48f045de0d8dbbd521e
mahdi.sarjan <mahdisarjan2@gmail.com>
2026-02-09 11:37:03 +0330

    created first app
    created first app named website

---
+++ website/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index_view(request):
+    return HttpResponse('<h1>home page<h1>')
+
+
+def about_view(request):
+    return HttpResponse('<h1>about page<h1>')
+
+
+def contact_view(request):
+    return HttpResponse('<h1>contact page<h1>') 
